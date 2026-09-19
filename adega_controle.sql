fb2394ac8c22fe49479bf8751f633edc5a663cdf
André Monteiro <andretalaricom1@gmail.com>
2026-09-19 17:36:41 -0300

    Edita produtos e cria fornecedores no SQL

--- adega_controle.sql
+++ adega_controle.sql
@@ -1,14 +1,37 @@
 CREATE DATABASE IF NOT EXISTS adega_controle;
 
-Use adega_controle;
+USE adega_controle;
 
-CREATE TABLE IF NOT EXISTS
-produtos (
-id INT AUTO_INCREMENT PRIMARY KEY, 
-nome VARCHAR(100) NOT NULL, 
-categoria VARCHAR(50) NOT NULL,
-unidade VARCHAR(50) NOT NULL,
-preco DECIMAL(10,2) DEFAULT 0.00,
-quantidade INT DEFAULT 0,
-data_cadastro TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+
+-- ============================================
+-- TABELA DE PRODUTOS
+-- ============================================
+
+CREATE TABLE IF NOT EXISTS produtos (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    nome VARCHAR(100) NOT NULL,
+    categoria VARCHAR(50) NOT NULL,
+    unidade VARCHAR(50) NOT NULL,
+    preco DECIMAL(10,2) DEFAULT 0.00,
+    quantidade INT DEFAULT 0,
+    data_cadastro TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
+
+
+-- ============================================
+-- TABELA DE FORNECEDORES
+-- ============================================
+
+CREATE TABLE IF NOT EXISTS fornecedores (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    empresa VARCHAR(100) NOT NULL,
+    CNPJ VARCHAR(18) NOT NULL UNIQUE,
+    telefone VARCHAR(20),
+    consultor VARCHAR(100),
+    email VARCHAR(100),
+    data_cadastro DATE
+);
+
+USE adega_controle;
+
+SELECT * FROM fornecedores;
